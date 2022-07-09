0597a172a508a360943f874c5e6115f372280969
mohasal0101 <mohasal0101@gmail.com>
2022-07-09 16:21:44 +0300

    cards

--- app.js
+++ app.js
@@ -147,11 +147,12 @@ for(let i = 0; i < employees.length; i++) {
 Employee.prototype.card = function () {
     document.write(`<div class="card">
     <div class="card-body">
+        <img src="${this.imageUrl}" alt="">
         <h5 class="card-title">${this.fullName}</h5>
         <p class="card-text">${this.department}</p>
         <p class="card-text">${this.level}</p>
         <p class="card-text">${this.salary}</p>
-        <img src="${this.imageUrl}" alt="">
+        
     </div>
 </div>`);
 }
