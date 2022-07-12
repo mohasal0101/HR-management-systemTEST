53560bf591065b242f150f7da3c19668f9043349
mohasal0101 <mohasal0101@gmail.com>
2022-07-12 19:01:57 +0300

    employee cards

--- app.js
+++ app.js
@@ -101,21 +101,8 @@ for(let i = 0; i < employees.length; i++) {
 
 
 
-Employee.prototype.card = function () {
-    document.write(`<div class="card">
-    <div class="card-body">
-    <img src="${this.imageUrl}" alt="">
-        <h5 class="card-title">${this.fullName}</h5>
-        <p class="card-text">${this.department}</p>
-        <p class="card-text">${this.level}</p>
-        <p class="card-text">${this.salary}</p>
-        
-    </div>
-</div>`);
-}
-for(let i = 0; i < employees.length; i++) {
-    employees[i].card();
-}
+
+
 
 document.getElementById("card").appendChild(card);
 
---
+++ function.js
@@ -0,0 +1,16 @@
+Employee.prototype.card = function () {
+    document.write(`<div class="card">
+    <div class="card-body">
+    
+        <h5 class="card-title">${this.fullName}</h5>
+        <p class="card-text">${this.department}</p>
+        <p class="card-text">${this.level}</p>
+        <p class="card-text">${this.salary}</p>
+        <img src="${this.imageUrl}" alt="">
+        
+    </div>
+</div>`);
+}
+for(let i = 0; i < employees.length; i++) {
+    employees[i].card();
+}
